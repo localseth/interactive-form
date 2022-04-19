2b136116587361282e83fab43b14ae364f9e16c6
Seth M <97685638+localseth@users.noreply.github.com>
2022-04-19 10:43:28 -0500

    declare variables add helper functions add evenet listeners

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 console.log('test');
+
+//Declare variables
+const nameField = document.getElementById('name');
+const otherJob = document.getElementById('other-job-role');
+
+//Helper functions
+const focusOrBlur = (domElement, status) => {
+    if (status === 'focus'){
+    domElement.focus();
+    } else if (status === 'blur') {
+        domElement.blur();
+    }
+}
+
+const showHide = (domElement, displayStatus) => {
+    domElement.style.display = displayStatus;
+}
+
+//Event listeners
+window.addEventListener('load', () => {
+    focusOrBlur(nameField, 'focus');
+    showHide(otherJob, 'none');
+});
